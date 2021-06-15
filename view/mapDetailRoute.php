56cdb4a116afb77ab77202b20749c7f419618e06
Emily Melendez <emi.mci.95@gmail.com>
2021-06-15 15:15:23 -0600

    changed view of route maps

--- view/mapDetailRoute.php
+++ view/mapDetailRoute.php
@@ -3,14 +3,15 @@
 include_once 'public/headerSections.php';
 ?>
 
+<h2 id="titlesMap">
+  Seguí la ruta a tu destino</h2>
+<div id="map"></div>
+<div id="right-panel">
+  <p>Total Distance: <span id="total"></span></p>
+</div>
 
-    <div id="map"></div>
-    <div id="right-panel">
-      <p>Total Distance: <span id="total"></span></p>
-    </div>
+<!-- Async script executes immediately and must be after any DOM elements used in callback. -->
 
-    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-  
 <?php
 
 include_once 'public/footerSections.php';
