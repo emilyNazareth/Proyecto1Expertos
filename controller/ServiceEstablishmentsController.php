87a7105962ec4a520dc24daa15d59734da58e9ee
Your Name <you@example.com>
2021-06-28 10:25:03 -0600

    Service establishments controller(delete method)

--- controller/ServiceEstablishmentsController.php
+++ controller/ServiceEstablishmentsController.php
@@ -26,38 +26,44 @@ class ServiceEstablishmentsController
         require 'model/ServiceEstablishmentsModel.php';
         require 'controller/Euclides.php';
         $euclidesAlgorithm = new Euclides();
-        $serviceEstablishments = ServiceEstablishmentsModel::singleton();
+        $serviceEstablishments = ServiceEstablishmentsModel::singleton();        
+        /* Get routes acoording to origin province anda final province*/
         $resultDataBaseSet = $serviceEstablishments->getServiceEstablisments($_POST['startingPoint'], $_POST['finalDestination']);
+        /* Get data selected to user*/
         $dataUserSelected['origin_province'] = $_POST['startingPoint'];
         $dataUserSelected['final_province'] = $_POST['finalDestination'];
         $dataUserSelected['duration_to_' . $_POST['startingPoint']] = $_POST['duration'];
         $dataUserSelected['distance_to_' . $_POST['startingPoint']] = $_POST['distance'];        
         $durationString = strval("duration_to_" . $_POST['startingPoint']);
         $distanceString = strval("distance_to_" . $_POST['startingPoint']);
+        /*change data values in order to use in euclides algorithm */
         foreach ($resultDataBaseSet as &$resultDb) {            
             $resultDb->$durationString = $euclidesAlgorithm->changeDurationValue($resultDb->$durationString);
             $resultDb->$distanceString = $euclidesAlgorithm->changeKmValue($resultDb->$distanceString);            
         }
-
+        /*call method to calculate euclides*/
         $routes = $euclidesAlgorithm->calculateDistance(
             $dataUserSelected,
             $resultDataBaseSet,
             [$durationString, $distanceString]
         );
         $arrayRoutes = json_decode(json_encode($routes), true);
+        /*order routes according differences values in ascending order*/
         asort($arrayRoutes);
+        /*create three routes using the differences where the ones with the 
+        smallest difference will be the most recommended*/
         $route1 = array_slice($arrayRoutes, 1, 3, true);
         $route2 = array_slice($arrayRoutes, 4, 3, true);
         $route3 = array_slice($arrayRoutes, 7, 3, true);
-        $routesInformation1 = $this->generateRoutes(
+        $routesInformation1 = $euclidesAlgorithm->generateRoutes(
             $resultDataBaseSet,
             $route1
         );
-        $routesInformation2 = $this->generateRoutes(
+        $routesInformation2 = $euclidesAlgorithm->generateRoutes(
             $resultDataBaseSet,
             $route2
         );
-        $routesInformation3 = $this->generateRoutes(
+        $routesInformation3 = $euclidesAlgorithm->generateRoutes(
             $resultDataBaseSet,
             $route3
         );
@@ -66,24 +72,6 @@ class ServiceEstablishmentsController
         $allRoutes['route2'] = $routesInformation3;
         echo  json_encode($allRoutes);
     }
-
-    public function generateRoutes($dataSetDataBase, $routesId)
-    {
-        $idsRoute = array_keys($routesId);
-        $routesInformation = [];
-        foreach ($dataSetDataBase as $result) {
-            foreach ($idsRoute as $resultId) {
-                if ($resultId == $result->id) {
-                    array_push($routesInformation, $result);
-                }
-            }
-        }
-        return   $routesInformation;
-    }
-
-
-
-
     /**
      * Get Service Establishments 
      */
